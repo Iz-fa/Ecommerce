b86954b320aa7bf6b3a91c2f20b667c4a850537c
Iz-fa <fastol35@gmail.com>
2026-01-20 00:18:33 +0100

    Create update delivery option feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -12,9 +12,10 @@ function App() {
   const [cart, setCart] = useState([]);
 
   const loadCart = async () => {
-
+    // The backend received 2 informations from the request: the type: get and the url path:/api/cart-items?expand=product
+    // The type is also called http method (get,post,put,delete)
     // ?expand is a query parameter. it lets us add additional info to our request
-    const response = await axios.get('/api/cart-items?expand=product')
+    const response = await axios.get('/api/cart-items?expand=product') 
     setCart(response.data);
   };
 
@@ -25,7 +26,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
-      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />} />
       <Route path="*" element={<ErrorPage cart={cart} />} />
