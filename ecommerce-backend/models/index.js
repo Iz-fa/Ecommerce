3ae64a96a6bddd62c5f315776dc40f3f1c0ceefe
Iz-fa <fastol35@gmail.com>
2026-02-26 23:26:02 +0100

    fix

--- ecommerce-backend/models/index.js
+++ ecommerce-backend/models/index.js
@@ -1,29 +1,7 @@
 import { Sequelize } from 'sequelize';
-import fs from 'fs';
 
-
-export let sequelize;
-
-sequelize = new Sequelize({
+export const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'database.sqlite',
   logging: false
 });
-
-// Save database to file after write operations.
-sequelize.addHook('afterCreate', saveDatabaseToFile);
-sequelize.addHook('afterDestroy', saveDatabaseToFile);
-sequelize.addHook('afterUpdate', saveDatabaseToFile);
-sequelize.addHook('afterSave', saveDatabaseToFile);
-sequelize.addHook('afterUpsert', saveDatabaseToFile);
-sequelize.addHook('afterBulkCreate', saveDatabaseToFile);
-sequelize.addHook('afterBulkDestroy', saveDatabaseToFile);
-sequelize.addHook('afterBulkUpdate', saveDatabaseToFile);
-
-
-export async function saveDatabaseToFile() {
-  const dbInstance = await sequelize.connectionManager.getConnection();
-  const binaryArray = dbInstance.database.export();
-  const buffer = Buffer.from(binaryArray);
-  fs.writeFileSync('database.sqlite', buffer);
-}
